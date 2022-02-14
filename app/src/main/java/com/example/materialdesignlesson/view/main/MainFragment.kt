c88e27d0188d7f30885261a7db171f9775be8dba
MatU <mattves@bk.ru>
2022-02-14 14:37:18 +0300

    [Lesson 1] добавлена возможность определиния состояния bottomSheet

--- app/src/main/java/com/example/materialdesignlesson/view/main/MainFragment.kt
+++ app/src/main/java/com/example/materialdesignlesson/view/main/MainFragment.kt
@@ -3,9 +3,12 @@ package com.example.materialdesignlesson.view.main
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.load
@@ -14,30 +17,60 @@ import com.example.materialdesignlesson.databinding.FragmentMainBinding
 import com.example.materialdesignlesson.view.BaseFragment
 import com.example.materialdesignlesson.viewmodel.PODData
 import com.example.materialdesignlesson.viewmodel.PODViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
-
 
 
     private val viewModel: PODViewModel by lazy {
         ViewModelProvider(this).get(PODViewModel::class.java)
     }
 
+    lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getData().observe(viewLifecycleOwner, Observer { renderData(it)
+        viewModel.getData().observe(viewLifecycleOwner, Observer {
+            renderData(it)
         })
         viewModel.sendRequest()
 
         binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
-                })
+                data =
+                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+            })
         }
+        //задание настроек для bottomSheet
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.included.bottomSheetContainer)
+
+        // открытие bottomSheet на половину
+        //bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+
+        //фиксированные состояния bottomSheet
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> myToast("состояние DRAGGING")
+                    BottomSheetBehavior.STATE_COLLAPSED -> myToast("состояние COLLAPSED")
+                    BottomSheetBehavior.STATE_EXPANDED -> myToast("состояние EXPANDED")
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> myToast("состояние HALF_EXPANDED")
+                    BottomSheetBehavior.STATE_HIDDEN -> myToast("состояние HIDDEN")
+                    BottomSheetBehavior.STATE_SETTLING -> myToast("состояние SETTLING")
+                }
+            }
+
+            //указывает уровень открытия bottomSheet через slideOffset
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("log", "slide $slideOffset")
+            }
+
+        })
     }
 
-    fun renderData(podData: PODData) {
+    private fun renderData(podData: PODData) {
         when (podData) {
             is PODData.Error -> {
 
@@ -46,7 +79,7 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
 
             }
             is PODData.Success -> {
-                binding.imageView.load(podData.serverResponse.url){
+                binding.imageView.load(podData.serverResponse.url) {
                     placeholder(R.drawable.ic_no_photo_vector)
 
                 }
@@ -60,4 +93,7 @@ class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::infl
         fun newInstance() = MainFragment()
     }
 
+    fun myToast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
 }
