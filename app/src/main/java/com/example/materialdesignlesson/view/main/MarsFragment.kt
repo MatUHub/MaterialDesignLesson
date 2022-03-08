cbb382e67b8320ac09a7d45ebdaccdcaf61056cd
MatU <mattves@bk.ru>
2022-03-08 12:10:03 +0300

    [Lesson 5] создана анимация взрыва кнопок при нажатию на одну из них. Кнопка на которую нажали остается

--- app/src/main/java/com/example/materialdesignlesson/view/main/MarsFragment.kt
+++ app/src/main/java/com/example/materialdesignlesson/view/main/MarsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.materialdesignlesson.view.main
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.*
 import com.example.materialdesignlesson.R
 import com.example.materialdesignlesson.databinding.FragmentMarsBinding
 import com.example.materialdesignlesson.view.BaseFragment
@@ -24,20 +26,45 @@ class MarsFragment : BaseFragment<FragmentMarsBinding>(FragmentMarsBinding::infl
         fun newInstance() = MarsFragment()
     }
 
-    class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    inner class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
-        class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+        inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_item_animation, parent, false))
+            return MyViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recycler_item_animation, parent, false)
+            )
         }
 
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-         holder.itemView.setOnClickListener{
-
-         }
+            holder.itemView.setOnClickListener {
+                val transitionSet = TransitionSet()
+                val explode = Explode()
+                val fade = Fade()
+                fade.duration = 99999999
+                explode.duration = 3000
+                val rect1 = Rect()
+                explode.excludeTarget(it, true)
+                it.getGlobalVisibleRect(rect1)
+                val rect2 = Rect(
+                    it.x.toInt(),
+                    it.y.toInt(),
+                    it.x.toInt() + it.width,
+                    it.y.toInt() + it.height
+                )
+                explode.epicenterCallback = object : Transition.EpicenterCallback() {
+                    override fun onGetEpicenter(transition: Transition): Rect {
+                        return rect1
+                    }
+                }
+                transitionSet.addTransition(explode)
+                transitionSet.addTransition(fade)
+                TransitionManager.beginDelayedTransition(binding.containerAnimation, transitionSet)
+                binding.recyclerView.adapter = null
+            }
         }
 
         override fun getItemCount(): Int {
