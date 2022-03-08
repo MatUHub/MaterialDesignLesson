08816b0cfa3dd0a4aa9ebf52a3aa9bb03ef5936e
MatU <mattves@bk.ru>
2022-03-08 19:20:37 +0300

    [Lesson 5] добавлен floatActionButton c всплывающим текстом по нажатию на нее и изменением видимости картинки

--- app/src/main/java/com/example/materialdesignlesson/view/main/EarthFragment.kt
+++ app/src/main/java/com/example/materialdesignlesson/view/main/EarthFragment.kt
@@ -1,5 +1,8 @@
 package com.example.materialdesignlesson.view.main
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
@@ -19,12 +22,14 @@ class EarthFragment : BaseFragment<FragmentEarthBinding>(FragmentEarthBinding::i
         fun newInstance() = EarthFragment()
     }
 
+    private var flag = false
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         var flag = false
 
-        binding.buttonAnimation.setOnClickListener{
+        binding.buttonAnimation.setOnClickListener {
 
             flag = !flag
 
@@ -36,7 +41,7 @@ class EarthFragment : BaseFragment<FragmentEarthBinding>(FragmentEarthBinding::i
 
             val params = binding.buttonAnimation.layoutParams as FrameLayout.LayoutParams
             binding.buttonAnimation.layoutParams = params
-            params.gravity = if(flag){
+            params.gravity = if (flag) {
                 Gravity.BOTTOM or Gravity.END
             } else {
                 Gravity.TOP or Gravity.START
@@ -44,7 +49,7 @@ class EarthFragment : BaseFragment<FragmentEarthBinding>(FragmentEarthBinding::i
 
             val params1 = binding.buttonAnimation1.layoutParams as FrameLayout.LayoutParams
             binding.buttonAnimation1.layoutParams = params1
-            params1.gravity = if(flag){
+            params1.gravity = if (flag) {
                 Gravity.BOTTOM or Gravity.START
             } else {
                 Gravity.TOP or Gravity.END
@@ -52,13 +57,138 @@ class EarthFragment : BaseFragment<FragmentEarthBinding>(FragmentEarthBinding::i
 
             val params2 = binding.buttonAnimation2.layoutParams as FrameLayout.LayoutParams
             binding.buttonAnimation2.layoutParams = params2
-            params2.gravity = if(flag){
+            params2.gravity = if (flag) {
                 Gravity.BOTTOM or Gravity.CENTER
             } else {
                 Gravity.TOP or Gravity.CENTER
             }
+        }
 
 
+
+        binding.containerOne.alpha = 0f
+        binding.containerTwo.alpha = 0f
+        binding.containerThree.alpha = 0f
+        binding.containerFour.alpha = 0f
+
+        binding.picEarth.alpha = 1f
+
+        binding.containerOne.isClickable = false
+        binding.containerTwo.isClickable = false
+        binding.containerThree.isClickable = false
+        binding.containerFour.isClickable = false
+
+        binding.fab.setOnClickListener {
+            flag = !flag
+
+            val duration = 1000L
+            if (flag) {
+                binding.picEarth.animate().alpha(0f)
+                binding.containerOne.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerOne.isClickable = true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.containerTwo.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerTwo.isClickable = true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.containerThree.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerThree.isClickable = true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.containerFour.animate()
+                    .alpha(1f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerFour.isClickable = true
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                ObjectAnimator.ofFloat(binding.iconPlus, View.ROTATION, 0f, 400f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerOne, View.TRANSLATION_Y, 0f, -130f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerThree, View.TRANSLATION_Y, 0f, 130f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerTwo, View.TRANSLATION_X, 0f, 220f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerFour, View.TRANSLATION_X, 0f, -220f)
+                    .setDuration(duration).start()
+            } else {
+                ObjectAnimator.ofFloat(binding.iconPlus, View.ROTATION, 400f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerOne, View.TRANSLATION_Y, -130f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerThree, View.TRANSLATION_Y, 130f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerTwo, View.TRANSLATION_X, 220f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.containerFour, View.TRANSLATION_X, -220f, 0f)
+                    .setDuration(duration).start()
+
+
+                binding.picEarth.animate().alpha(1f)
+
+                binding.containerOne.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerOne.isClickable = false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.containerTwo.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerTwo.isClickable = false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.containerThree.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerThree.isClickable = false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+
+                binding.containerFour.animate()
+                    .alpha(0f)
+                    .setDuration(duration)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            binding.containerFour.isClickable = false
+                            super.onAnimationEnd(animation)
+                        }
+                    })
+            }
         }
     }
 
